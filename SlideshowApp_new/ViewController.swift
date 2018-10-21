1e9ee9ab6b0e16e8cdc2eead4e3731546759320b
tk1110 <t.kameda.1110@gmail.com>
2018-10-21 19:19:30 +0900

    Initial Commit

---
+++ SlideshowApp_new/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  SlideshowApp_new
+//
+//  Created by Takaaki Kameda on 2018/10/21.
+//  Copyright © 2018年 takaaki.kameda. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
